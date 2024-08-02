4a005c2c25c14788ee044089946f5244f49466fa
German <gercger5@gmail.com>
2024-08-02 17:30:29 -0300

    Sidebar with routing

---
+++ src/app/countries/countries.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ByCapitalPageComponent } from './pages/by-capital-page/by-capital-page.component';
+import { ByCountryPageComponent } from './pages/by-country-page/by-country-page.component';
+import { ByRegionComponent } from './pages/by-region/by-region.component';
+import { CountryPageComponent } from './pages/country-page/country-page.component';
+
+
+
+@NgModule({
+  declarations: [
+    ByCapitalPageComponent,
+    ByCountryPageComponent,
+    ByRegionComponent,
+    CountryPageComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CountriesModule { }
